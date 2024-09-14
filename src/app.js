e1a1e50e54562194b7f6f88b85afdcffef7a8481
Omkar Raykar <raykaromkar1244@gmail.com>
2024-09-14 20:29:55 +0530

    add path and boilerplate code of routes and controllers

--- src/app.js
+++ src/app.js
@@ -16,12 +16,26 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
 // routes import
-import userRouter from './routes/userRoutes.js'
-
+import userRouter from "./routes/userRoutes.js";
+import healthcheckRouter from "./routes/healthcheckRoutes.js";
+import tweetRouter from "./routes/tweetRoutes.js";
+import subscriptionRouter from "./routes/subscriptionRoutes.js";
+import videoRouter from "./routes/videoRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import likeRouter from "./routes/likeRoutes.js";
+import playlistRouter from "./routes/playlistRoutes.js";
+import dashboardRouter from "./routes/dashboardRoutes.js";
 
 // routes declaration
-app.use("/users", userRouter)
+app.use("/users", userRouter);
+app.use("/healthcheck", healthcheckRouter);
+app.use("/tweets", tweetRouter);
+app.use("/subscriptions", subscriptionRouter);
+app.use("/videos", videoRouter);
+app.use("/comments", commentRouter);
+app.use("/likes", likeRouter);
+app.use("/playlist", playlistRouter);
+app.use("/dashboard", dashboardRouter);
 
 export { app };
